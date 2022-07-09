e10f796db63d998339e1d94cf08a079c2a731d10
Wralith <wralithdev@gmail.com>
2022-07-09 20:19:14 +0300

    Add solution 0876

---
+++ leetcode/0876-middle-of-linked-list/middle-of-linked-list.go
@@ -0,0 +1,28 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	if head.Next == nil || head == nil {
+		return head
+	}
+
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
---
+++ leetcode/0876-middle-of-linked-list/middle-of-linked-list_test.go
@@ -0,0 +1,65 @@
+package leetcode
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// from github.com/halfrost
+func Ints2List(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	l := &ListNode{}
+	t := l
+	for _, v := range nums {
+		t.Next = &ListNode{Val: v}
+		t = t.Next
+	}
+	return l.Next
+}
+
+func List2Ints(head *ListNode) []int {
+	limit := 100
+
+	times := 0
+
+	res := []int{}
+	for head != nil {
+		times++
+		if times > limit {
+			msg := fmt.Sprintf("链条深度超过%d，可能出现环状链条。请检查错误，或者放宽 l2s 函数中 limit 的限制。", limit)
+			panic(msg)
+		}
+
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+// from github.com/halfrost
+
+func Test_middleNode(t *testing.T) {
+	tests := []struct {
+		name string
+		got  []int
+		want []int
+	}{
+		{
+			name: "Example 1",
+			got:  []int{1, 2, 3, 4, 5},
+			want: []int{3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := List2Ints(middleNode(Ints2List(tt.got))); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("middleNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
